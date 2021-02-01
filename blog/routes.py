8149199388eac4db22c5deff043b84aa5c31498f
Marcin <bizior.marcin@googlemail.com>
2021-02-01 02:07:01 +0100

    login setup, draft page created, hiden if not logged set

--- blog/routes.py
+++ blog/routes.py
@@ -1,7 +1,8 @@
-from flask import Flask, request, render_template, redirect, flash
+from flask import Flask, request, render_template, redirect, flash, session, url_for
 from blog import app
 from blog.models import Entry, db
-from blog.forms import EntryForm
+from blog.forms import EntryForm, LoginForm
+import functools
 
 
 @app.route("/")
@@ -10,7 +11,18 @@ def index():
     return render_template("homepage.html", all_posts=all_posts)
 
 
+def login_required(view_func):
+    @functools.wraps(view_func)
+    def check_permissions(*args, **kwargs):
+        if session.get("logged_in"):
+            return view_func(*args, **kwargs)
+        return redirect(url_for("login", next=request.path))
+
+    return check_permissions
+
+
 @app.route("/post/", methods=["GET", "POST"])
+@login_required
 def entry():
     form = EntryForm()
     errors = None
@@ -45,6 +57,49 @@ def entry():
             errors = form.errors
 
     return render_template("entry_form.html", form=form, errors=errors)
+
+
+@app.route("/login/", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    errors = None
+    next_url = request.args.get("next")
+    if request.method == "POST":
+        if form.validate_on_submit():
+            session["logged_in"] = True
+            session.permanent = True  # Use cookie to store session.
+            flash("You are now logged in.", "success")
+            return redirect("/")
+        else:
+            errors = form.errors
+    return render_template("login_form.html", form=form, errors=errors)
+
+
+@app.route("/logout/", methods=["GET", "POST"])
+def logout():
+    if request.method == "POST":
+        session.clear()
+        flash("You are now logged out.", "success")
+    return redirect("/")
+
+
+@app.route("/drafts/", methods=["GET", "POST"])
+def drafts():
+    all_drafts = Entry.query.filter_by(is_published=False).order_by(Entry.pub_date.desc())
+    return render_template("drafts.html", all_drafts=all_drafts)
+
+
+@app.route("/delete/<int:entry_id>")
+def delete_entry(entry_id):
+    entry = Entry.query.get(entry_id)
+    errors = None
+    if not entry:
+        return redirect("/")
+
+    db.session.delete(entry)
+    db.session.commit()
+    flash("Post Deleted.", "success")
+    return redirect("/")
 
 
 @app.route("/contact/", methods=["GET", "POST"])
--- config.py
+++ config.py
@@ -1,3 +1,6 @@
+import os
+
+
 class Config(object):
     DEBUG = True
     DEVELOPMENT = True
@@ -5,6 +8,8 @@ class Config(object):
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = "sqlite:///posts.db"
     SECRET_KEY = "do-i-really-need-this"
+    ADMIN_USERNAME = os.environ.get("ADMIN_USERNAME", "")
+    ADMIN_PASSWORD = os.environ.get("ADMIN_PASSWORD", "")
 
 
 class ProductionConfig(Config):
